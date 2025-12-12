bb768b40424979f6d8376bfa2c41498ce7e8cd91
lakenMaigalano <20246327@s.ubaguio.edu>
2025-12-12 20:15:22 +0800

    first commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,59 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Firestore, collection, collectionData, addDoc, doc, deleteDoc, updateDoc } from
+'@angular/fire/firestore';
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+selector: 'app-root',
+standalone: true,
+imports: [CommonModule, FormsModule],
+templateUrl: './app.html',
+styleUrls: ['./app.css']
 })
 export class App {
-  protected readonly title = signal('galano_appdev');
+title = signal('APPDEV Angular with Firebase Demonstration with Student Management System');
+
+studentName = signal('');
+studentCourse = signal('')
+studentEmail = signal('')
+studentUsername = signal('')
+studentPassword = signal ('')
+students: any[] = [];
+
+constructor(private firestore: Firestore) {
+const studentsCollection = collection(this.firestore, 'students');
+collectionData(studentsCollection, { idField: 'id' })
+.subscribe(data => {
+this.students = data; // Assign to array so Angular detects changes
+});
+}
+
+addStudent() {
+const name = this.studentName();
+const course = this.studentCourse();
+const email = this.studentEmail();
+const username = this.studentUsername();
+const password = this.studentPassword();
+if (name && course && email && username && password) {
+const studentsCollection = collection(this.firestore, 'students');
+addDoc(studentsCollection, { name, course, email, username, password });
+this.studentName.set('');
+this.studentCourse.set('');
+this.studentEmail.set('');
+this.studentUsername.set('');
+this.studentPassword.set('');
+
+}
+}
+
+deleteStudent(id: string) {
+const studentDoc = doc(this.firestore, `students/${id}`);
+deleteDoc(studentDoc);
+}
+
+updateStudent(id: string, newName: string,  newCourse: string, newEmail: string, newUsername: string, newPassword: string) {
+const studentDoc = doc(this.firestore, `students/${id}`);
+updateDoc(studentDoc, { name: newName,  course: newCourse, email: newEmail, username: newUsername,  password: newPassword});
+}
 }
---
+++ src/environment/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+production: false,
+firebase: {
+apiKey: "AIzaSyCs7qXGTEsOecdy8YxHzyHYoi0sSLH4tqU",
+authDomain: "appdev-a9c67.firebaseapp.com",
+projectId: "appdev-a9c67",
+storageBucket: "appdev-a9c67.firebasestorage.app",
+messagingSenderId: "925596673200",
+appId: "1:925596673200:web:c2bb9a5db9880b2350c775"
+}
+};
